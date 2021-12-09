16d1fc1e76f1bc3d62ef96c413bea2b8cde1a6db
Alexey Pesotsky <pesthedog@mail.ru>
2021-12-09 19:53:36 +0400

    resolve name conflict in param name

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -2,17 +2,17 @@ import requests
 from utils import download_image, get_extension_from_url
 
 
-def fetch_spacex_launch(id="latest"):
-    url = f"https://api.spacexdata.com/v4/launches/{id}"
+def fetch_spacex_launch(launch_id="latest"):
+    url = f"https://api.spacexdata.com/v4/launches/{launch_id}"
 
     response = requests.get(url)
     response.raise_for_status()
 
     images = response.json()["links"]["flickr"]["original"]
 
-    for image_index, image in enumerate(images):
+    for image_index, image in enumerate(images, 1):
         image_extension = get_extension_from_url(image)
         download_image(
             image,
-            f"images/spacex/spacex_{image_index + 1}{image_extension}"
+            f"images/spacex/spacex_{image_index}{image_extension}"
         )
